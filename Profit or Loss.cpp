f3c41cbfcfad0649d01728b277f04f574229a51a
sudarsinianisetti <sudarsinianisetti@gmail.com>
2025-12-08 16:37:09 +0530

    Add files via upload

---
+++ Profit or Loss.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int c,s;
+    scanf("%d%d",&c,&s);
+    if(c<s)
+    {
+        printf("Profit");
+    }
+    else if(c>s)
+    {
+        printf("Loss");
+    }
+    else
+    {
+        printf("No Profit and No Loss");
+    }
+}
