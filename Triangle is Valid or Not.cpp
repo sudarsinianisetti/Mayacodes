e0f909488f2b3392af19318a836f5b20c26a02fa
sudarsinianisetti <sudarsinianisetti@gmail.com>
2025-12-08 16:43:32 +0530

    Add files via upload

---
+++ Triangle is Valid or Not.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(c<(a+b)&&b<(a+c)&&a<(b+c))
+    {
+        printf("Valid triangle");
+    }
+    else
+    {
+        printf("Invalid triangle");
+    }
+}
