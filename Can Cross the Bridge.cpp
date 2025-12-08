889e991c1fe0b4ce228c2d6292f23db745533897
sudarsinianisetti <sudarsinianisetti@gmail.com>
2025-12-08 19:48:31 +0530

    Add files via upload

---
+++ Can Cross the Bridge.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+// Constrained code starts here
+    int x,y,z,r;
+    scanf("%d%d%d",&x,&y,&z);
+
+// Constrained code ends here
+    // Write your logic here
+    r=(z-y)/x;
+// Constrained code starts here
+    printf("%d",r);
+
+// Constrained code ends here
+}
